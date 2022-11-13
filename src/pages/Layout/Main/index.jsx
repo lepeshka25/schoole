61aa6ae35bec7249cfea950d10655f590df8c424
lepeshka25 <ruslanmuratbekov1972@gmail.com>
2022-11-13 16:07:30 +0600

    finish contact

--- src/pages/Layout/Main/index.jsx
+++ src/pages/Layout/Main/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Swiper from "./components/swiper";
 import cs from './style.module.scss'
+import Contacts from "./components/Contacts";
 
 const Main = () => {
 	return (
 		<div className={cs.main}>
 			<div className={cs.swiper_container}>
 				<Swiper/>
+			</div>
+			<div className={cs.container_contacts}>
+				<Contacts/>
 			</div>
 		</div>
 	);
